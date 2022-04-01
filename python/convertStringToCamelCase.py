c69a2d445f4490f63e17557cf5891e8b64d8f9fe
Jonathan Cole <joncole2014@gmail.com>
2022-03-31 20:36:15 -0500

    Link to problem: https://www.codewars.com/kata/517abf86da9663f1d2000003

--- python/convertStringToCamelCase.py
+++ python/convertStringToCamelCase.py
@@ -1,3 +1,11 @@
+# Complete the method/function so that it converts dash/underscore delimited 
+# words into camel casing. The first word within the output should be 
+# capitalized *only* if the original word was capitalized.
+#
+# ex: 'the-stealth-warrior' gets converted to 'theStealthWarrior'
+#
+# Link to problem: https://www.codewars.com/kata/517abf86da9663f1d2000003
+
 def to_camel_case(text):
     
     out = ""   # empty string for output
